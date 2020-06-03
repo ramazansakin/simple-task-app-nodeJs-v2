8eed37f614b7b5cb40e5801d4bdd758ad7f4ab40
r84160880 <ramazan.sakin@huawei.com>
2020-06-03 13:06:15 +0300

    adding security api for authorizing the apis we want

---
+++ src/middleware/auth.js
@@ -0,0 +1,27 @@
+// --------------------------------------------------
+// Authorization api - promise
+// If you wanna authorize any api, you can cross cut it via this api
+// and chech the client has valid token or not
+
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '')
+        const decoded = jwt.verify(token, 'thisismynewcourse')
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
+
+        if (!user) {
+            throw new Error()
+        }
+
+        req.token = token
+        req.user = user
+        next()
+    } catch (e) {
+        res.status(401).send({ error: 'Please authenticate.' })
+    }
+}
+
+module.exports = auth
