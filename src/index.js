2c21333eab2ab12ebd4a0f9b46ef9e6196df6d54
r84160880 <ramazan.sakin@huawei.com>
2020-06-03 13:07:10 +0300

    creating index file and dependency file to drive app

---
+++ src/index.js
@@ -0,0 +1,30 @@
+// --------------------------------------------------
+// Index file
+// Manages and drives the all pieces of codes here
+
+const express = require('express')
+require('./db/mongoose')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
+
+const Task = require('./models/task')
+const User = require('./models/user')
+
+const main = async () => {
+    const user = await User.findById('5c2e4dcb5eac678a23725b5b')
+    await user.populate('tasks').execPopulate()
+    console.log(user.tasks)
+}
+
+main()
